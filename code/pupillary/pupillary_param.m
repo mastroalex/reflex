0fc82c7b9a02372a46b9d1911ae1f121b2d3acb3
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-06-05 11:47:39 +0200

    model parameters correction

--- code/pupillary/pupillary_param.m
+++ code/pupillary/pupillary_param.m
@@ -1,9 +1,8 @@
 % parameters for pupillary reflex
 % parameters must be corrected
-D=5;
-K1=10;
-tau=0.5;
+D=0.18;
+K1=0.16;
+tau=0.1;
 num=[K1];
 den_prod=[tau 1];
 den=conv(conv(den_prod,den_prod),den_prod);
-% tf(num,den)
